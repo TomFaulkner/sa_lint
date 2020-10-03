98e6b369d82fba32d4e75d801c09bd656b330169
Tom Faulkner <tomfaulkner@gmail.com>
2020-10-02 20:23:23 -0500

    examples, tests, and poetry

---
+++ examples/redefined.py
@@ -0,0 +1,23 @@
+class Boolean:
+    pass
+
+
+class Column:
+    def __init__(self, type_=None):
+        pass
+
+
+class Integer:
+    pass
+
+
+class RedefinedColumnsModel:
+    __table_name__ = "redefined_columns_model"
+    duplicate = Column(Integer)
+    col_1 = Column()
+    col_2 = Column()
+    duplicate = Column(Boolean)
+
+
+false_positive = True
+false_positive = False
---
+++ tests/test_redefined.py
@@ -0,0 +1,43 @@
+import pytest
+
+from sa_lint.sa_lint import lint_redefined_columns, DuplicateFinder
+
+data = """
+from sqlalchemy import Boolean, Column, Integer
+
+class RedefinedColumnsModel:
+    __table_name__ = 'redefined_columns_model'
+    duplicate = Column(Integer)
+    col_1 = Column()
+    col_2 = Column()
+    duplicate = Column(Boolean)
+
+false_positive = True
+false_positive = False
+"""
+
+
+@pytest.mark.asyncio
+async def test_lint_redefined_columns():
+    res = await lint_redefined_columns(data)
+    assert len(res) == 1
+    assert "duplicate" in res
+    assert res["duplicate"][0] == (6, "Column(Integer)")
+    assert res["duplicate"][1] == (9, "Column(Boolean)")
+    assert "false_positive" not in res
+
+
+def test_duplicate_finder():
+    nums = [1, 2, 3, 1, 2, 3, 4, 5, 6]
+    df = DuplicateFinder()
+    for i, n in enumerate(nums):
+        df[n] = i
+    assert 4 not in df.duplicates
+    assert 5 not in df.duplicates
+    assert 6 not in df.duplicates
+    assert 1 in df.duplicates
+    assert 2 in df.duplicates
+    assert 3 in df.duplicates
+
+    for n in nums:
+        assert n in df.items
